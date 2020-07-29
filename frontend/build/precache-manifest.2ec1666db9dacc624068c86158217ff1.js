5afc552a738724ae9e0f5070923a4a2a51000121
jctrilamanila@gmail.com <jctrilamanila@gmail.com>
2020-07-29 14:27:36 -0400

    fixed nested repo added React

---
+++ frontend/build/precache-manifest.2ec1666db9dacc624068c86158217ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a293d08b7caa8969ca5de69f6b83ab3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6815d991099b1573e7e1",
+    "url": "/static/css/main.3e574635.chunk.css"
+  },
+  {
+    "revision": "9fd84bf897b618a176ab",
+    "url": "/static/js/2.6e29a2ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e29a2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6815d991099b1573e7e1",
+    "url": "/static/js/main.c2feae14.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+export default {
+    products: [
+        {
+        name: 'Nike Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpeg',
+        price: 40,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 12
+    },
+      {
+        name: 'Nike Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpeg',
+        price: 25,
+        brand: 'Nike',
+        rating: 4.3,
+        numReviews: 5
+    },
+      {
+        name: 'Nike Track Pants',
+        category: 'Pants',
+        image: '/images/d1.jpeg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.1,
+        numReviews: 9
+    },
+      {
+        name: 'Nike Track Pants',
+        category: 'Pants',
+        image: '/images/d1.jpeg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 17
+    },
+      {
+        name: 'Nike Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpeg',
+        price: 30,
+        brand: 'Nike',
+        rating: 3,
+        numReviews: 1
+    },
+    ]
+}
